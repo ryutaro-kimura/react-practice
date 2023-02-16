3026741628a9644fa664c1ddb50c64a9de57eff7
ryutaro-kimura <65991131+ryutaro-kimura@users.noreply.github.com>
2023-02-16 05:37:48 +0000

    ToDoListコンポーネント

---
+++ src/components/ToDoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TodoList() {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
